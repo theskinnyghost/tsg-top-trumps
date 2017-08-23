5e2209b4eb0ef7d550816630ed00cd2b4d05d49a
Luca Ricci <theskinnyghost@gmail.com>
2017-08-23 10:47:22 +0200

    Initial commit

---
+++ tsg-top-trumps.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name:       TSG Top Trumps
+ * Plugin URI:        https://github.com/theskinnyghost/tsg-top-trumps
+ * Description:       This plugin allows you to play Top Trumps on your WordPress site.
+ * Version:           1.0.0
+ * Author:            Luca Ricci
+ * Author URI:        https://www.michiamoluca.it
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
