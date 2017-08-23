03309ee92ce6d950c2bb135b931978c68cca0d16
Luca Ricci <theskinnyghost@gmail.com>
2017-08-23 14:35:48 +0200

    Add frontend markup and styles

--- views/template-top-trumps.php
+++ views/template-top-trumps.php
@@ -10,7 +10,35 @@
 
 <body <?php body_class(); ?>>
 
-<div id="content" class="game-canvas"></div>
+<div class="game-canvas">
+
+    <h1 class="main-title"><?php echo __( 'Football stars Top Trumps', 'tsg_top_trumps' ); ?></h1>
+
+    <button class="start-game"><?php echo __( 'Play game', 'tsg_top_trumps' ); ?></button>
+
+    <div class="response"></div>
+
+    <div class="cards-holder">
+        <div class="card">
+            <div class="card-inner">
+                <div class="card-face card-face--back">
+                    <img class="top-trumps-logo" src="<?php echo plugin_dir_url( dirname( __FILE__ ) ) . 'assets/Top_Trumps.svg'; ?>">
+                </div>
+                <div class="card-face card-face--front">
+                    <h2 class="card-name"></h2>
+                    <img class="card-image">
+                    <ul class="card-list">
+                        <li class="card-prop">
+                            <span></span>
+                            <span></span>
+                        </li>
+                    </ul>
+                    <div class="card-description"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
 <?php wp_footer(); ?>
 </body>
